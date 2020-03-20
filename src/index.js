b49d66ab4a051889229707495c84afb9117036f4
Chris Bitsakis <c.bitsakis@gmail.com>
2020-03-20 01:26:37 -0400

    jxa

--- src/index.js
+++ src/index.js
@@ -1,35 +1,66 @@
-const osa = require('osa2')
 const youtubedl = require('youtube-dl')
 const axios = require('axios')
-const exec = require('child_process').exec;
+const exec = require('child_process').exec
+const jxa = require('jxa')
+var Application = jxa.Application
+const Music = Application('Music')
+const QuickTime = Application('QuickTime Player')
 
+const youtubeSearchApiUrl = 'https://www.googleapis.com/youtube/v3/search'
+const youtubeApiKey = 'AIzaSyAiMxG_IKPU_mQvis-M5c0tK7_9T91yu74'
+let currentTrack = ''
 
-osa(() => {
-  return Application('iTunes').currentTrack.name() + ' ' + Application('iTunes').currentTrack.artist()
-})().then(async (q) => {
-  const youtubeApiKey = 'AIzaSyD9MRuCFYkMOKeJn_bv78e59oF5IxgFKQQ'
+const currentTrackArtist = () => Music.currentTrack.artist()
+const currentTrackName = () => Music.currentTrack.name()
+const getCurrentTrack = () => `${currentTrackName()} ${currentTrackArtist()}`
+const updateCurrentTrack = () => currentTrack = getCurrentTrack()
 
-  axios.get(`https://www.googleapis.com/youtube/v3/search`, {
+updateCurrentTrack()
+
+const playVideo = async currentTrack => {
+  console.log(`play video for: ${currentTrack}`)
+  const { data: { items } } = await axios.get(youtubeSearchApiUrl, {
     params: {
       part: 'snippet',
       key: youtubeApiKey,
-      q: `${q} video`
+      q: `${currentTrack} video`
     }
-  }).then(({ data }) => {
-    const { items } = data
-    const noResults = items.length === 0
-
-    if (noResults) return;
-
-    const firstResult = items[0]
-    const { videoId } = firstResult.id
-    const video = youtubedl(`http://www.youtube.com/watch?v=${videoId}`,
-      ['--format=18'],
-      { cwd: __dirname }
-    );
-
-    video.on('info', ({ url }) => {
-      exec(`/Applications/IINA.app/Contents/MacOS/iina-cli "${url}"`)
-    });
   })
-}).then(() => {}).catch(console.error)
+
+  const noResults = items.length === 0
+  if (noResults) return
+
+  const firstResult = items[0]
+  const { videoId } = firstResult.id
+
+  console.log(`found video id ${videoId}`)
+
+  const video = youtubedl(`http://www.youtube.com/watch?v=${videoId}`,
+    ['--format=22,17,18'],
+    { cwd: __dirname }
+  )
+
+  video.on('info', ({ url }) => {
+    console.log('got video info. opening quicktime...')
+    // exec(`/Applications/IINA.app/Contents/MacOS/iina-cli "${url}"`)
+    QuickTime.openURL(url)
+    // Bring QuickTime to the forefront
+    // setTimeout(() => QuickTime.activate(), 200)
+  })
+}
+
+const stopCurrentVideo = () => {
+  QuickTime.windows[0].document.close()
+}
+
+playVideo(currentTrack)
+
+setInterval(() => {
+  if (currentTrack === getCurrentTrack()) return
+
+  console.log(`track changed: ${getCurrentTrack()}`)
+  updateCurrentTrack()
+
+  stopCurrentVideo()
+  playVideo(currentTrack)
+}, 1000)
